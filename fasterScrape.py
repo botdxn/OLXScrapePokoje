cfe554eb95eeb2d73d17e308b781e138608dbb7f
Marcin Kucharski <dixone23@gmail.com>
2019-09-24 17:58:22 +0200

    File is now being saved with current date and pageNum value. Added multithreading with ThreadPoolExecutor for multiple times faster processing. To-do: add threading for filling up the cache (list of links).

--- fasterScrape.py
+++ fasterScrape.py
@@ -1,33 +1,65 @@
-import bs4, requests, smtplib, time
+import bs4, requests, time, threading, concurrent.futures, re, os
+from datetime import datetime
 
-pageNum = 10
+pageNum = 5
 baseURL = 'https://www.olx.pl/nieruchomosci/stancje-pokoje/wroclaw/?page='
+URLList = []
 
 def scrapeAll():
-    totalOgloszenia = 0
     for x in range (1, pageNum):
+        print(f"Tworzenie cache linków ogłoszeń...{x}")
+        os.system('cls')
         requestURL = requests.get(baseURL + str(x))
         print(f"{baseURL + str(x)} status: {requestURL.status_code}")
         requestURL.encoding = 'utf-8'
         soupParser = bs4.BeautifulSoup(requestURL.text, 'html.parser')
         ogloszenia = soupParser.find_all('table', {'summary': 'Ogłoszenie'})
 
-        with open('data.txt', 'a', encoding="UTF-8") as file:
-            for ogloszenie in ogloszenia:
-                totalOgloszenia += 1
-                cena = ogloszenie.find('p', {'price'}).text
-                cena = cena.replace(" ", "")
-                cena = cena.replace("zł", "")
-                cena = cena.replace("\t", "").replace("\r", "").replace("\n", "")
+        for ogloszenie in ogloszenia:
+            link = ogloszenie.find('a', {'marginright5'})['href']
 
-                tytul = ogloszenie.find('a', {'data-cy': 'listing-ad-title'}).text
-                tytul = str(tytul.replace("\t", "").replace("\r", "").replace("\n", ""))
+            if str(link) in URLList:
+                pass
+            else:
+                URLList.append(link)
 
-                link = ogloszenie.find('a', {'marginright5'})['href']
-                link = link.replace("\t", "").replace("\r", "").replace("\n", "")
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = executor.map(requestSingle, URLList)
 
-                file.write(tytul+'%'+cena+'%'+link+'\n')
+def requestSingle(link):
+    now = datetime.now()
+    dt_string = now.strftime("%d-%m-%Y")
+    filename = 'data_' + str(dt_string) + '_' + str(pageNum) + '.txt'
+    with open(filename, 'a', encoding="UTF-8") as file:
+        try:
+            typPokoju = ''
+            requestSingle = requests.get(link)
+            print(f"{link} status: {requestSingle.status_code}")
+            requestSingle.encoding = 'utf-8'
 
-    print(totalOgloszenia)
+            soupParser = bs4.BeautifulSoup(requestSingle.text, 'html.parser')
+            try:
+                typPokoju = soupParser.find_all('table', {'class': 'item'})[2]
+                typPokoju = typPokoju.find('a').text
+                typPokoju = typPokoju.replace("\t", "").replace("\r", "").replace("\n", "")
+            except:
+                typPokoju = "Brak danych"
+
+            cena = soupParser.find('div', {'class': 'price-label'}).text
+            cena = cena.replace(" ", "")
+            cena = cena.replace("zł", "")
+            cena = cena.replace("\t", "").replace("\r", "").replace("\n", "")
+
+            tytul = soupParser.find('div', {'class': 'offer-titlebox'})
+            tytul = tytul.find('h1').text
+            tytul = str(tytul.replace("\t", "").replace("\r", "").replace("\n", ""))
+            tytul = tytul.strip()
+
+
+            file.write(tytul+'%'+cena+'%'+link+'%'+str(typPokoju)+'\n')
+            file.close()
+            os.system('cls')
+        except:
+            pass
 
 scrapeAll()
